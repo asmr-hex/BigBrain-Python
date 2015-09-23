db82fb537c2decf67d0a6af1a80a1cc21b1a1b68
Connor Walsh <connorrobinsonwalsh@gmail.com>
2015-09-23 23:02:58 +0000

    initial commit

---
+++ motor.py
@@ -0,0 +1,13 @@
+
+
+class motor:
+    """Handles the detection and configuration of the controller
+    system. 
+    """
+    
+    __handTypes = []
+
+    def __init__(self):
+        """Initializes list of controller types available."""
+        
+        
---
+++ readme.md
@@ -0,0 +1,6 @@
+# hands
+A Python package which handles (no pun intended) the use of custom built video game controllers 
+for BigBrain.
+
+#Liscense
+MIT copyright [Connor Walsh](http://connorwalsh.net)
